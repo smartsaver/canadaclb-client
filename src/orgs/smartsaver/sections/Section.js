deeef9121d1fc1dbaf4f0c74849d18a13df27762
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-01-24 17:30:09 -0500

    Add Smartsaver Section component
    Specifically for smarsaver only

---
+++ src/orgs/smartsaver/sections/Section.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Section from '../../../components/Section'
+
+const SmartsaverSection = ({ children }) => {
+  return <Section className="SmartsaverSection">{children}</Section>
+}
+
+export default SmartsaverSection
