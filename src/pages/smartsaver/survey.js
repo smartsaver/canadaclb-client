926f273210be34d55622855eac2e2f6fa513ca49
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-01-24 22:56:32 -0500

    Add route page component
    Enabled /smartsaver/survey route only along with index and 404 page

---
+++ src/pages/smartsaver/survey.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SmartsaverLayout from '../../orgs/smartsaver/Layout'
+
+const surveyPage = () => {
+  return (
+    <SmartsaverLayout>
+      <h1 className="title">Smartsaver Survey</h1>
+    </SmartsaverLayout>
+  )
+}
+
+export default surveyPage
