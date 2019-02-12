938400d1bb158c3d4031ceca1d3818ef17c2d449
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-12 13:06:21 -0500

    Add logo to navbar
    building ufile page

--- src/orgs/smartsaver/sections/ufileresp/UfilerespLayout.js
+++ src/orgs/smartsaver/sections/ufileresp/UfilerespLayout.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Layout from '../../../../components/Layout'
+import Navbar from '../../../../components/Navbar'
+import NavbarItem from '../../../../components/Navbar/NavbarItem'
+import logo from '../../images/ufile-logo.png'
 
 const UfilerespLayout = ({ children }) => {
   return (
     <Layout>
+      <Navbar
+        navbarBrand={
+          <NavbarItem
+            text={<img className="image" src={logo} alt="UfileRESP" />}
+          />
+        }
+      />
       <main>{children}</main>
     </Layout>
   )
