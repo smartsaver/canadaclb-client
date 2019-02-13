d9d79485d122e7f012369212fe464fc879e89465
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-13 00:19:34 -0500

    Modify Content
    Modify url embed links for iframes

--- src/orgs/educavenir/content/impotexpertreee/applyContent.js
+++ src/orgs/educavenir/content/impotexpertreee/applyContent.js
@@ -15,7 +15,7 @@ Des recherches démontrent que de petits montants mis de côté pour les études
 
 3. Les enfants ayant de l’épargne-études entre 1 $ et 499 $ sont trois fois plus susceptibles de poursuivre des études postsecondaires et quatre fois plus susceptibles d’obtenir leur diplôme.
   `),
-  embedUrl: 'http://reeeussite.ca/Impotexpert',
+  embedUrl: 'https://www.educavenir.org/reeeussite/',
   footerContent: processMarkdown(
     "Présenté par UFile et SmartSAVER. Pour plus d'information contactez 1-888-RESP-CLB ou info@smartsaver.org"
   ),
