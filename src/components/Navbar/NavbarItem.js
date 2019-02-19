1ac0f4ca1560d4de016d7b28e86772be95b6931f
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-19 14:43:15 -0500

    Add navbarItem component docs

--- src/components/Navbar/NavbarItem.js
+++ src/components/Navbar/NavbarItem.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+/**
+ * @function NavbarItem
+ * @param text - Any text or react element
+ * @param {string} href - link or url
+ * @param {boolean} isLinkWillOpenInNewWindow - will put target="_blank" and rel="noopenner noreferrer" to a tag when true
+ */
+
 const NavbarItem = ({ text, href, isLinkWillOpenInNewWindow = false }) => {
   const linkTagOptions = !isLinkWillOpenInNewWindow
     ? {}
