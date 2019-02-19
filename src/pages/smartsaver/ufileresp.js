194d8edea5390fd91b79ef5486ee50f96e14a596
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-19 15:29:49 -0500

    Fix height for iframe survey embed

--- src/pages/smartsaver/ufileresp.js
+++ src/pages/smartsaver/ufileresp.js
@@ -3,6 +3,7 @@ import Layout from '../../orgs/smartsaver/sections/ufileresp/UfilerespLayout'
 import Section from '../../orgs/smartsaver/sections/ufileresp/Section'
 import Banner from '../../orgs/smartsaver/sections/ufileresp/Banner'
 import SurveyForm from '../../components/SurveyForm'
+import ApplySection from '../../orgs/smartsaver/sections/ufileresp/ApplySection'
 import UfileVideoSection from '../../orgs/smartsaver/sections/ufileresp/UfileVideo'
 import allContent from '../../orgs/smartsaver/content/ufileresp/index'
 
@@ -17,30 +18,13 @@ const IntroSection = () => {
   )
 }
 
-const ApplySection = () => {
-  const { content, embedUrl, footerContent, title } = applyContent
-  return (
-    <Section name="apply">
-      <h2 className="title is-3">{title}</h2>
-      <div className="content" dangerouslySetInnerHTML={{ __html: content }} />
-      <SurveyForm isEnabled={true}>
-        <iframe src={embedUrl} title="UFile RESP Application" />
-      </SurveyForm>
-      <div
-        className="content"
-        dangerouslySetInnerHTML={{ __html: footerContent }}
-      />
-    </Section>
-  )
-}
-
 const UfilerespPage = () => {
   return (
     <Layout>
       <Banner content={bannerContent} />
       <UfileVideoSection url={bannerContent.videoUrl} />
       <IntroSection />
-      <ApplySection />
+      <ApplySection content={applyContent} />
     </Layout>
   )
 }
