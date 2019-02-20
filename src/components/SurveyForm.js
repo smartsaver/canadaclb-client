4babbfdd43fda9b06b304bdb16dbe88b2ee5f47a
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-20 15:09:38 -0500

    Edit Section content for apply

--- src/components/SurveyForm.js
+++ src/components/SurveyForm.js
@@ -9,10 +9,10 @@ import './SurveyForm.css'
  * @param {string} overlayText
  */
 
-const SurveyForm = ({ isEnabled, children, overlayText = '' }) => {
+const SurveyForm = ({ isEnabled, children, overlayText = '', name = '' }) => {
   const isFormDisabled = !isEnabled ? 'SurveyForm--disabled' : ''
   return (
-    <div className={`SurveyForm ${isFormDisabled}`}>
+    <div className={`SurveyForm ${isFormDisabled}`} id={name}>
       {children}
       <span className="SurveyForm__overlay has-text-danger">{overlayText}</span>
     </div>
