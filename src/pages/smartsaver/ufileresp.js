b633cef3192852e7d39dd483f36b74a99c5d6251
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-20 00:04:17 -0500

    Add Footer Section and footer content

--- src/pages/smartsaver/ufileresp.js
+++ src/pages/smartsaver/ufileresp.js
@@ -3,9 +3,10 @@ import Layout from '../../orgs/smartsaver/sections/ufileresp/UfilerespLayout'
 import Banner from '../../orgs/smartsaver/sections/ufileresp/Banner'
 import ApplySection from '../../orgs/smartsaver/sections/ufileresp/ApplySection'
 import UfileVideoSection from '../../orgs/smartsaver/sections/ufileresp/UfileVideo'
+import FooterSection from '../../orgs/smartsaver/sections/ufileresp/FooterSection'
 import allContent from '../../orgs/smartsaver/content/ufileresp/index'
 
-const { introContent, applyContent, bannerContent } = allContent
+const { introContent, applyContent, bannerContent, footerContent } = allContent
 
 const UfilerespPage = () => {
   return (
@@ -13,6 +14,7 @@ const UfilerespPage = () => {
       <Banner content={bannerContent} />
       <UfileVideoSection url={bannerContent.videoUrl} content={introContent} />
       <ApplySection content={applyContent} />
+      <FooterSection content={footerContent} />
     </Layout>
   )
 }
