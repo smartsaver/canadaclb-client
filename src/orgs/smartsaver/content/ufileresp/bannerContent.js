84a92dc2250e46c19add575dfa8d11396bd4e777
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-20 00:21:12 -0500

    Change Banner content

--- src/orgs/smartsaver/content/ufileresp/bannerContent.js
+++ src/orgs/smartsaver/content/ufileresp/bannerContent.js
@@ -1,11 +1,8 @@
-import processMarkdown from '../../../../libs/marked'
-
 const bannerContent = {
   intro: 'Thanks to UFile for Supporting Education Savings!',
   title: 'Grow your child’s dreams for tomorrow!',
-  content: processMarkdown(`
-Did you know that youth with savings for their educations are 50% more likely to participate in post-secondary education than those who have none?
-  `),
+  content:
+    'Did you know that youth with savings for their educations are 50% more likely to participate in post-secondary education than those who have none?',
   videoUrl: 'https://www.youtube.com/embed/ev6ZtfjRdBg',
 }
 
