3c2ee9dfeb2663bbb666729a86390fe687ed8398
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-20 23:30:09 -0500

    Add SEO for /educavenir/impotexpertreee

--- src/pages/educavenir/impotexpertreee.js
+++ src/pages/educavenir/impotexpertreee.js
@@ -4,6 +4,7 @@ import Banner from '../../orgs/educavenir/sections/impotexpertreee/Banner'
 import IntroSection from '../../orgs/educavenir/sections/impotexpertreee/IntroSection'
 import ApplySection from '../../orgs/educavenir/sections/impotexpertreee/ApplySection'
 import FooterSection from '../../orgs/smartsaver/sections/ufileresp/FooterSection'
+import SEO from '../../components/Seo'
 import allContent from '../../orgs/educavenir/content/impotexpertreee/index'
 
 const { applyContent, introContent, bannerContent, footerContent } = allContent
@@ -11,6 +12,12 @@ const { applyContent, introContent, bannerContent, footerContent } = allContent
 const ImpotexpertreeePage = () => {
   return (
     <Layout>
+      <SEO
+        title="ImpôtExpert"
+        description={bannerContent.seoContent}
+        lang="fr"
+        keywords={['reee', 'éducation', 'ImpôtExpert']}
+      />
       <Banner content={bannerContent} />
       <IntroSection content={introContent} />
       <ApplySection content={applyContent} />
