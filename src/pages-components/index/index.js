2a96318743ac8331516a7b2daa81130a65f21027
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-22 15:02:40 -0500

    Add footer parameters and default arguments
    for smartsaver footer

--- src/pages-components/index/index.js
+++ src/pages-components/index/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import Navbar from '../smartsaver/SmartsaverNavbar'
 import Footer from '../smartsaver/SmartsaverFooter'
+import SmartsaverNavbar from '../smartsaver/SmartsaverNavbar'
+import NavbarItem from '../../components/Navbar/NavbarItem'
+
+const Navbar = () => {
+  return (
+    <SmartsaverNavbar
+      navbarEnd={<NavbarItem text="Contact" href="#contact" />}
+    />
+  )
+}
 
 const IndexPage = () => {
   return (
