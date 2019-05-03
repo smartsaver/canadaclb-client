6cafffac579b5335d8b632dbb0a895cbc0c36464
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-05-03 18:16:47 -0400

    Add VideoSection and Title Meta tag

--- src/pages-components/rocketpiggy/index.js
+++ src/pages-components/rocketpiggy/index.js
@@ -4,12 +4,18 @@ import Layout from './Layout'
 import Banner from './RocketPiggyBanner'
 import contentList from './content'
 import ApplySection from './ApplySection'
+import VideoSection from './VideoSection'
+import SEO from '../../components/Seo/index'
 
 const RocketPiggyPage = () => {
   return (
     <Layout>
-      <Banner content={contentList.bannerContent} />
-      <ApplySection content={contentList.applyContent} />
+      <SEO title="RocketPiggy" />
+      <main>
+        <Banner content={contentList.bannerContent} />
+        <VideoSection url="https://www.youtube.com/embed/ev6ZtfjRdBg" />
+        <ApplySection content={contentList.applyContent} />
+      </main>
     </Layout>
   )
 }
