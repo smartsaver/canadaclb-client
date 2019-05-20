ac25e30d6fc15028b1451be01be810f2f317b44a
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-05-20 01:25:35 -0400

    Add Container to video section

--- src/pages-components/ywca/IndexPage/VideoSection.js
+++ src/pages-components/ywca/IndexPage/VideoSection.js
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography'
 import grey from '@material-ui/core/colors/grey'
 import withStyles from '@material-ui/core/styles/withStyles'
 
-const styles = () => ({
+const styles = theme => ({
   root: {
     textAlign: 'center',
     backgroundColor: grey[800],
@@ -21,28 +21,34 @@ const styles = () => ({
     lineHeight: '1.9rem',
     color: 'white',
   },
+  container: {
+    maxWidth: theme.breakpoints.values.xl,
+    margin: '0 auto',
+  },
 })
 
 function VideoSection(props) {
   const { classes, content = { title: '' } } = props
   return (
     <Section className={classes.root}>
-      <Typography
-        variant="h2"
-        classes={{
-          root: classes.title,
-        }}
-      >
-        {content.title}
-      </Typography>
-      <iframe
-        className={classes.frame}
-        title="Youtube Video"
-        src={props.url}
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen={true}
-      />
+      <div className={classes.container}>
+        <Typography
+          variant="h2"
+          classes={{
+            root: classes.title,
+          }}
+        >
+          {content.title}
+        </Typography>
+        <iframe
+          className={classes.frame}
+          title="Youtube Video"
+          src={props.url}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen={true}
+        />
+      </div>
     </Section>
   )
 }
