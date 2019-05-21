7c5e0bd405b700c610ab1b5db1730f33c3068812
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-05-21 15:01:40 -0400

    Add Bacgkround color for odd section

--- src/pages-components/ywca/Layout/index.js
+++ src/pages-components/ywca/Layout/index.js
@@ -4,11 +4,15 @@ import Navbar from '../components/Navbar/index'
 import NavbarBrand from '../components/Navbar/NavbarBrand'
 import Footer from '../components/Footer/index'
 import withStyles from '@material-ui/core/styles/withStyles'
+import grey from '@material-ui/core/colors/grey'
 
 const styles = () => ({
   '@global': {
     html: {
       scrollBehavior: 'smooth',
+    },
+    'section:nth-child(odd)': {
+      backgroundColor: grey[100],
     },
   },
 })
