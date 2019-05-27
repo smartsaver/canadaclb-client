ceb9db20f4c030803bf9f731d01be8fe490bf034
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-05-27 17:20:44 -0400

    Add Overlay Background for the Resp Application

--- src/pages-components/ywca/IndexPage/ApplySection/index.js
+++ src/pages-components/ywca/IndexPage/ApplySection/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Section from '../components/Section'
+import Section from '../../components/Section'
 import Typograpy from '@material-ui/core/Typography'
+import Overlay from './Overlay'
 import withStyles from '@material-ui/core/styles/withStyles'
 
 const styles = theme => ({
-  root: {},
   container: {
     maxWidth: theme.breakpoints.values.xl,
     margin: '0 auto',
@@ -42,12 +42,14 @@ function ApplySection(props) {
         >
           <div dangerouslySetInnerHTML={{ __html: content.text }} />
         </Typograpy>
-        <iframe
-          className={classes.frame}
-          src={content.embedUrl}
-          frameBorder="0"
-          title="StartMyResp Application"
-        />
+        <Overlay>
+          <iframe
+            className={classes.frame}
+            src={content.embedUrl}
+            frameBorder="0"
+            title="StartMyResp Application"
+          />
+        </Overlay>
         <Typograpy
           variant="body1"
           component="div"
