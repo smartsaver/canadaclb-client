c0af2d432d08a93fb48c5cab96ffd4abb818a525
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-07 15:32:59 -0400

    Change Banner Text

--- src/pages-components/ywca/IndexPage/content/bannerContent.js
+++ src/pages-components/ywca/IndexPage/content/bannerContent.js
@@ -4,9 +4,9 @@ const bannerContent = {
   text: marked(`
 Did you know that youth with savings for their education are 50% more likely to participate in post-secondary education than those who have none?
 
-Learn how you can turn your child’s dream into a reality through the YWCA Accelerator Program! 
-
 The YWCA is a proud supporter of educational savings that can help make your child’s future bright!
+
+Apply today to the YWCA Accelerator Program!
   `),
 }
 
