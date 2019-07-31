6b36f752211481f78a36d0d5d67d18333962271d
jcunanan05 <j_cunanan05@yahoo.com>
2019-07-31 13:55:56 -0400

    Update YWCA content

--- src/pages-components/ywca/IndexPage/content/bannerContent.js
+++ src/pages-components/ywca/IndexPage/content/bannerContent.js
@@ -5,9 +5,7 @@ const bannerContent = {
 Did you know that youth with savings for their education are 50% more likely to participate in post-secondary education than those who have none?
 
 The YWCA is a proud supporter of educational savings that can help make your child’s future bright!
-
-Apply today to the YWCA Accelerator Program and you could get an incentive!
-  `),
+`),
 }
 
 export default bannerContent
