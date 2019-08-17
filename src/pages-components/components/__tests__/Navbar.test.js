dca2cc577d4cdb8c8709bce36f00a8b0d84c209a
jcunanan05 <j_cunanan05@yahoo.com>
2019-08-17 19:25:44 -0400

    Add Navbar Drawer Tests

--- src/pages-components/components/__tests__/Navbar.test.js
+++ src/pages-components/components/__tests__/Navbar.test.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '../Navbar'
-import { render } from '@testing-library/react'
+import NavbarDrawer from '../Navbar/NavbarDrawer'
+import { ListItem } from '@material-ui/core'
+import { render, fireEvent } from '@testing-library/react'
 
 describe('Navbar', () => {
   test('Navbar Without Drawer renders with brand, menu, and burger bar', () => {
@@ -16,5 +18,28 @@ describe('Navbar', () => {
     expect(getByText(brandText))
     expect(getByText(menuText))
     expect(getByLabelText(burgerBar))
+  })
+
+  test('Navbar + Navbar Drawer appears on burger click', async () => {
+    const [brandText, menuText, drawerText, burgerBar] = [
+      'fakebrandText',
+      'fakemenuText',
+      'navbarDrawerText',
+      'Menu',
+    ]
+    const { getByLabelText, getByText } = render(
+      <Navbar
+        brand={brandText}
+        menuEnd={menuText}
+        renderDrawer={({ isOpen, closeDrawer }) => (
+          <NavbarDrawer isOpen={isOpen} onClose={closeDrawer}>
+            <ListItem>{drawerText}</ListItem>
+          </NavbarDrawer>
+        )}
+      />
+    )
+
+    fireEvent.click(getByLabelText(burgerBar))
+    expect(getByText(drawerText))
   })
 })
