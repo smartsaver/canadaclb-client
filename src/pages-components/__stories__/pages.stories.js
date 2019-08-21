5985513dc41a5541c5bd3180302e3ee5920e8c87
jcunanan05 <j_cunanan05@yahoo.com>
2019-08-21 13:48:52 -0400

    Add new page
    not in live site yet. Prototyping

---
+++ src/pages-components/__stories__/pages.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import TaxIndexPage from '../tax/IndexPage'
+
+storiesOf('/tax', module).add('Index Page', () => <TaxIndexPage />)
---
+++ src/pages-components/tax/Layout/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+/**
+ * @param {*} children
+ */
+
+function Layout({ children }) {
+  return <main>{children}</main>
+}
+
+export default Layout
